dc53a1214774b3e5823c6567e9a0dfaad867d3be
vinoddive <divevinod8767@gmail.com>
2026-01-22 18:29:03 +0530

    Division Program

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main(String[] args){
+        int a=10;
+        int b=4;
+        int d=a/b;
+        System.out.println("Division is :"+ d);
+    }
+}
