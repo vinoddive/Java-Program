955af08166f0cdae7ca0d27c51960af81979afa7
vinoddive <divevinod8767@gmail.com>
2026-01-22 18:23:08 +0530

    Multiplication of two numbers

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args){
+        int p=10;
+        int q=5;
+        int r = p * q;
+        System.out.println("Multiplication is "+ r);
+    }
+}
