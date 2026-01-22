36b898a37f3f21a7f220d52ff07d4b9ce4d4f759
vinoddive <divevinod8767@gmail.com>
2026-01-22 18:18:55 +0530

    Substraction of two numbers

---
+++ Subtraction.java
@@ -0,0 +1,8 @@
+public class Subtraction {
+    public static void main(String[] args){
+        int x=100;
+        int y=50;
+        int z=x-y;
+        System.out.println("Subtraction of two numbers " + z);
+    }
+}
