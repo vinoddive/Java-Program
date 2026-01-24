371f9e648220ac2ec4bd60b1d53266d46cb98e7b
vinoddive <divevinod8767@gmail.com>
2026-01-24 22:45:56 +0530

    Number in word

---
+++ NumberInWord.java
@@ -0,0 +1,24 @@
+public class NumberInWord {
+    public static void main(String[] args){
+        int n=10;
+
+        if(n==1){
+            System.out.println("One");
+        }
+        else if(n==2){
+            System.out.println("two");
+        }
+        else if(n==3){
+            System.out.println("three");
+        }
+        else if(n==4){
+            System.out.println("four");
+        }
+        else if(n==5){
+            System.out.println("Five");
+        }
+        else{
+            System.out.println("Invalid Number");
+        }
+    }
+}
