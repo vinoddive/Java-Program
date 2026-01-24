8c6671e5c19e4dc9a405b4a1256651b3ac6eb1d6
vinoddive <divevinod8767@gmail.com>
2026-01-24 22:14:24 +0530

    if else statement example

---
+++ if_else_example.java
@@ -0,0 +1,11 @@
+public class if_else_example {
+    public static void main(String[] args){
+        int n = -1;
+        if(n >= 0){
+            System.out.println("Positive Number");
+        }
+        else{
+            System.err.println("Negative Number");
+           }
+    }
+}
