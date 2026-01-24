4567ca1a44a6b2e80c997da1e9794bacfb160a80
vinoddive <divevinod8767@gmail.com>
2026-01-24 22:20:57 +0530

    Check number even or odd using scanner class

---
+++ even_or_odd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class even_or_odd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+
+        if(n % 2 == 0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
+        }
+    }
+    
+}
