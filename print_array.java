f65d9ced4b2ea6bf49fad154834de5410544d813
vinoddive <divevinod8767@gmail.com>
2026-01-25 19:27:37 +0530

    Print array

---
+++ print_array.java
@@ -0,0 +1,10 @@
+public class print_array {
+    public static void main(String[] args){
+        int [] arr = {12,10,5,3,2};
+
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
