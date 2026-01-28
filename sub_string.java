95d18dd27b6b8fea0bf9617920b3254b956dee4b
vinoddive <divevinod8767@gmail.com>
2026-01-28 22:43:48 +0530

    use substring function and cute string two parts

---
+++ sub_string.java
@@ -0,0 +1,7 @@
+public class sub_string {
+    public static void main(String[] args){
+        String s = "Programming";
+        System.out.println(s.substring(0,5));
+        System.out.println(s.substring(3));
+    }
+}
