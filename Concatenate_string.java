2264c5c3d10be1c27f86de91fd7581b52b9e4af7
vinoddive <divevinod8767@gmail.com>
2026-01-28 22:28:13 +0530

    Add two strings

---
+++ Concatenate_string.java
@@ -0,0 +1,8 @@
+public class Concatenate_string {
+    public static void main(String[] args){
+        String s1 = "Hello";
+        String s2 = "Java";
+        String s3 = s1 +" "+ s2;
+        System.out.println(s3);
+    }
+}
