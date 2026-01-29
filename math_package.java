2102f6e147ae7148360ac3928edbd21b8cb5075a
vinoddive <divevinod8767@gmail.com>
2026-01-29 22:28:19 +0530

    use math package methods

---
+++ math_package.java
@@ -0,0 +1,8 @@
+public class math_package {
+    public static void main(String[] args){
+        
+        System.out.println(Math.addExact(10, 5));
+        System.out.println(Math.subtractExact(10, 5));
+        System.out.println(Math.multiplyExact(10, 5));
+    }
+}
