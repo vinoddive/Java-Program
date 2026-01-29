38b3b3f7d2b9340a6f60869197e4db0823c4da38
vinoddive <divevinod8767@gmail.com>
2026-01-29 22:25:21 +0530

    use date package methods

---
+++ date_method.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+public class date_method {
+    public static void main(String[] args){
+        Date d = new Date();
+
+    System.out.println(d.getDate());     
+    System.out.println(d.getMonth());    
+    System.out.println(d.getYear());  
+    }
+}
