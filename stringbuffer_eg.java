397e1fd5a522cb8c48115600ab5865cc8b1957a9
vinoddive <divevinod8767@gmail.com>
2026-01-29 22:05:23 +0530

    Use String Buffer class

---
+++ stringbuffer_eg.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class stringbuffer_eg {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("Hello ");
+
+        sb.append("Java");
+
+        System.out.println(sb);
+    }
+}
