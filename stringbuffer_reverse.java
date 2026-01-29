6e8394ceb956a0a87c703785490627fc4d3e16ad
vinoddive <divevinod8767@gmail.com>
2026-01-29 22:13:34 +0530

    Use reverse methos in string Buffer class

---
+++ stringbuffer_reverse.java
@@ -0,0 +1,8 @@
+public class stringbuffer_reverse {
+    public static void main(String[] args){
+        StringBuffer sb = new StringBuffer("Program");
+
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
