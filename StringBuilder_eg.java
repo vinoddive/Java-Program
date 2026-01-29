f4eaaf2a0e6342fa705844ef7a32dbb8de8f3bfa
vinoddive <divevinod8767@gmail.com>
2026-01-29 22:31:29 +0530

    StringBuilder class example

---
+++ StringBuilder_eg.java
@@ -0,0 +1,7 @@
+public class StringBuilder_eg {
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("Hi");
+        sb.append(" Java");
+        System.out.println(sb);
+    }
+}
