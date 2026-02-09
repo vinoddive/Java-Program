f1005319acadfb2fee2176d57758e1a5566aa2cb
vinoddive <divevinod8767@gmail.com>
2026-02-09 22:51:34 +0530

    Calculate square of number

---
+++ print_square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class print_square {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any Number");
+        int n = sc.nextInt();
+
+        int sqr = n*n;
+        System.out.println("Sqaure of number :-" + sqr);
+    }
+}
