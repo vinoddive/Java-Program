3366a6d8569b34f41c598ab1e93b10ee0247bfa2
vinoddive <divevinod8767@gmail.com>
2026-02-13 23:43:59 +0530

    Copy Constructor Example

---
+++ copy_construct.java
@@ -0,0 +1,29 @@
+class Student {
+    int id;
+    String name;
+
+    
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    
+    Student(Student s) {
+        id = s.id;
+        name = s.name;
+    }
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+}
+public class copy_construct {
+    public static void main(String[] args){
+         Student s1 = new Student(101, "Vinod");
+        Student s2 = new Student(s1);
+
+        s1.display();
+        s2.display();
+    }
+}
